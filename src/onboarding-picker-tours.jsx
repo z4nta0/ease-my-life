1a4181eed724f5883640ed03b50b4d3eb77219de
z4nta0 <z4nta0@gmail.com>
2026-08-20 11:24:37 -0500

    Bold Relax intro modal and reword Step 9 closing sentence

--- src/onboarding-picker-tours.jsx
+++ src/onboarding-picker-tours.jsx
@@ -69,12 +69,12 @@ const PICKER_TOUR_COPY = {
     step10Body: <>This controls the <b>maximum number of days that a workout item must wait before it should be picked again</b>. This is also useful since you usually want a workout to be picked again within a certain timeframe.</>,
   },
   pkr_ob_relax: {
-    body2: 'This tutorial will guide you through creating a Relax picker. This type of picker is an Ease-down and is perfect for activities you want to stick with for a few days at a time instead of changing every day. e.g. once it picks "Read a book", that activity will stay as the picked item for at least 5 days but no more than a week before a new activity is chosen. Let’s create one of these now.',
+    body2: <>This tutorial will guide you through creating a Relax picker. This type of picker is an Ease-down and is <b>perfect for activities you want to stick with for a few days at a time instead of changing every day</b>. e.g. once it picks "Read a book", that activity will stay as the picked item for at least 5 days but no more than a week before a new activity is chosen. Let’s create one of these now.</>,
     itemPrefill: 'Take a nap',
     itemSoonest: 3,
     itemLatest: 5,
     step8Body: <>This is the name of the activity item and is <b>what will show up in your todo list if it is picked</b>. We’ve already filled this out for you but feel free to customize it to whatever you’d prefer.</>,
-    step9Body: <>This controls the <b>minimum number of days that an activity item will stay picked before it discharges</b> and another item is picked. This is useful since most activities you want to stick with for a few days at a time instead of changing every day.</>,
+    step9Body: <>This controls the <b>minimum number of days that an activity item will stay picked before it discharges</b> and another item is picked. This is useful since most activities you want to stick with for a certain timeframe instead of changing every day.</>,
     step10Body: <>This controls the <b>maximum number of days that an activity item will stay picked before it discharges</b> and another item is picked. This is also useful since most activities you don’t want to stay picked past a certain timeframe.</>,
   },
 };
